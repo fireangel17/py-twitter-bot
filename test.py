e5fda02b8479a09aff5a8d593dcb693abab61deb
Fireangel <hallo@web.de>
2017-05-23 19:07:24 +0200

    ll

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from tweet_text import idle_text , reply
 # In unittest, test cases are represented by instances of unittest's TestCase class.
 # To make your own test cases you must write subclasses of TestCase.
 
-<<<<<<< HEAD
+
 class TestTweetText(unittest.TestCase):
 	def test_math(self):
 		response = reply({'text':'1+1', 'user': {'screen_name': 'TestUser'}})
@@ -22,18 +22,6 @@ class TestTweetText(unittest.TestCase):
         # methods provided by the TestCase base class
          #https://docs.python.org/2/library/unittest.html#unittest.TestCase.assertTrue
         #self.assertTrue(self.text.startswith('It is'))
-=======
-
-class TestTweetText(unittest.TestCase):
-
-    def test_idle(self):
-        # In order to test something, we use one of the assert*()
-        # methods provided by the TestCase base class
-        # https://docs.python.org/2/library/unittest.html#unittest.TestCase.assertTrue
-        self.assertTrue( isinstance(idle_text(), str) )
-
->>>>>>> 04ada90c694c3899890a50bfaaae154fb9b82217
-
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
